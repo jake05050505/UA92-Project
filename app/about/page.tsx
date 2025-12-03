c37c1a980a06d1917f3a10fdbaf3614c656315af
jake05050505 <jakehancox12@gmail.com>
2025-12-03 17:14:05 +0000

    add content to /about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,23 @@ import Header from "@/components/Header"
 
 export default function About(){
     return (
-        <Header />
+        <>
+            <Header />
+            <main>
+                <div id="main-content" className="content">
+                    <h1>About</h1>
+                    <hr></hr>
+                    <h2>The project</h2>
+
+                    <hr></hr>
+                    <h2>The research</h2>
+                    <h3>Withdrawal</h3>
+                    <h4>Your right to withdrawal</h4>
+                    <h5>How will your data be used if you decide to withdraw?</h5>
+                    <p>withdrawal information here</p>
+                    <hr></hr>
+                </div>
+            </main>
+        </>
     )
 }
