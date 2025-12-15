ac550910eafa79be398815400c90411b94ec1e0d
jake05050505 <jakehancox12@gmail.com>
2025-12-15 21:42:05 +0000

    express migrate 3, add section to /about

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "static")));
 
+
 const devMode = process.env.NODE_ENV !== "production";
 
 renderChatInterface = (req, res) => {
